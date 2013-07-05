a8d98181b808075052202a423c1ee5565e9883cb
Joshua Carp <jm.carp@gmail.com>
2013-07-05 12:19:44 -0400

    wrap in if __name___... to fix sphinx bug

--- setup.py
+++ setup.py
@@ -5,16 +5,20 @@ Install sciparse tools.
 # Imports
 from setuptools import setup, find_packages
 
-# Install
-setup(
-    name='sciparse',
-    version='0.1',
-    packages=find_packages(),
-    package_data={
-        '' : [
-            'tests/fixtures/citparse/*.json',
-            'tests/fixtures/refparse/*.json',
-        ]
-    },
-    include_package_data=True,
-)
+# 
+if __name__ == '__main__':
+
+    # Install
+    setup(
+        name='sciparse',
+        version='0.1',
+        packages=find_packages(),
+        package_data={
+            '' : [
+                'tests/fixtures/citparse/*.json',
+                'tests/fixtures/refparse/*.json',
+            ]
+        },
+        include_package_data=True,
+    )
+
